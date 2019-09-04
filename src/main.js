4c66f52dd1758a519249cc7af90528517600be30
mutohiroshi <mutohiroshi@team-lab.com>
2019-09-04 14:42:46 +0900

    [update] main.jsにてstoreの利用を可能に実装&App.vueの不要箇所削除

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import store from './vuex/store'
 
-Vue.config.productionTip = false
 
 new Vue({
+  el: '#app',
+
+  // コンポーネントからstoreの利用を可能にする
+  store,
   render: h => h(App),
 }).$mount('#app')
